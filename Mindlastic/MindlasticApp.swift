6f99adea7406d725fe522e28129b200edf4dd886
emurray2 <evan@auraaudio.io>
2021-10-23 13:10:12 -0400

    Initial Project

---
+++ Mindlastic/MindlasticApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindlasticApp.swift
+//  Mindlastic
+//
+//  Created by Evan Murray on 10/23/21.
+//
+
+import SwiftUI
+
+@main
+struct MindlasticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
