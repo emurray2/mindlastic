b59723bc8b45f2d036bc60aa3788fd420b767129
James-S-007 <jspringer8@gatech.edu>
2021-10-23 18:51:54 -0400

    add-journal endpoint

--- backend/server/config.py
+++ backend/server/config.py
@@ -1,14 +1,31 @@
+from bson.objectid import ObjectId
 import flask
+
+from database.MongodbClient import MongodbClient
 
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
+client = MongodbClient('localhost', 27017)
+uid = ObjectId('6174874509654612d212e150')
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Flask Test</h1>"
+    return '<h1>Flask Test</h1>'
 
-@app.route('/api/user', methods=['GET'])
-def user():
-    return "<h1>This is a user field</h1>"
+@app.route('/api/user/add-journal', methods=['POST'])
+def add_journals():
+    entry = request.data
+    # insert gtp3 call here
+    client.new_journal(uid, entry, 5, 'summary placeholder')
+
+@app.route('/api/user/get-journal',methods=['GET'])
+def get_journals()
+    uid = request.args.get('uid')
+    data = request.args.get('date')
+
+@app.route('/api/user/get-data', methods=['GET'])
+def get_user_Data():
+    uid = requests.args.get('uid')
+    
 
 app.run()
